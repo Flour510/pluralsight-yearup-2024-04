873acb4073b0f490ea50ebc538cd96a375be17eb
flour <99916123+Flour510@users.noreply.github.com>
2024-04-10 15:59:42 -0700

    Learing about Java

--- workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
+++ workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
@@ -7,35 +7,45 @@ public class Main
         // declare variables for each of the following:
 
         // a vehicle identification number in the range 1000000 - 9999999
-
+        int vehicleIdentificationNumber = 9999990;
+        System.out.println("VIN #: " + vehicleIdentificationNumber);
 
         // a vehicle make /model (i.e. Ford Explorer)
-
+        String makeOfVehicle = "Vokswagen ";
+        String modelOfVehicle = "Beetle";
+        System.out.println("Car type: " + makeOfVehicle + modelOfVehicle);
 
         // a vehicle color
-
+        String vehicleColor = "Red";
+        System.out.println("The vehicle is color: " + vehicleColor);
 
         // whether the vehicle has a towing package
-
+        boolean towingPackageAvailable = true;
+        System.out.println("Tow package available: " + towingPackageAvailable);
 
         // an odometer reading
-
+        int odometerReading = 510510510;
+        System.out.println("Odometer reading: " + odometerReading);
 
         // a price
-
+        double priceOfWater = 2.50;
+        System.out.println(priceOfWater);
 
         // a quality rating (A, B, or C)
-
+        char qualityRating = 'F';
+        System.out.println("Vehicle quality rating: " + qualityRating);
 
         // a phone number
-
+        String phoneNumber = "(510)866-8164";
+        System.out.println(phoneNumber);
 
         // a social security number
-
+        int socialSecurityNumber = 123445678;
+        System.out.println("SSN: " + socialSecurityNumber);
 
         // a zip code
-
-
+        int zipCode = 94601;
+        System.out.println("Zip code: " + zipCode);
 
     }
 }
---
+++ workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/StringConcatenation.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class StringConcatenation
+{
+    public static void main(String[] args)
+    {
+        String firstName= "Flor";
+        String lastName = "Preciado";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+}

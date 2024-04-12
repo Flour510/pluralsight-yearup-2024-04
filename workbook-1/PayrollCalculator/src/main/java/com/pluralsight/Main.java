83bf36f77f2babb7240b510fc22496f8de9fcbab
flour <99916123+Flour510@users.noreply.github.com>
2024-04-12 14:28:09 -0700

    Completed Mod3 -  Excercise 4 - Payroll Calculator

--- workbook-1/PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ workbook-1/PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        // create a scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // read name
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+
+        // read hours worked
+        System.out.print("How many hours did you work? ");
+        double hoursWorked = scanner.nextDouble();
+
+        // read pay rate
+        System.out.print("What is your pay rate? ");
+        double payRate = scanner.nextDouble();
+
+        // calculate pay rate / print gross pay
+        double grossPay = payRate * hoursWorked;
+        System.out.printf("%s, your gross pay is: $%.2f", name , grossPay);
+
     }
+
+
 }

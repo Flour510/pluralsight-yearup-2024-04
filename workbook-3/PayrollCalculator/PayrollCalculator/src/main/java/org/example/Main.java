7e8c3c5cb955952847e706c4e5688b05e7d2483c
flour <99916123+Flour510@users.noreply.github.com>
2024-04-25 08:44:14 -0700

    BedtimeStories Exercise

---
+++ workbook-3/PayrollCalculator/PayrollCalculator/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+
+    public static void employeeData(String[] args)
+    {
+
+        // get the specific variables
+        int employeeId = columns[0];
+        String name = columns[1];
+        double hoursWorked = Double.parseDouble(columns[2]);
+        double payRate = Double.parseDouble(columns[3]);
+
+    }
+}

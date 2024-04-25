306292ee2e25e4967ee779dbc26f641d3ac9972e
flour <99916123+Flour510@users.noreply.github.com>
2024-04-25 14:47:09 -0700

    PayrollCalculator

--- workbook-3/PayrollCalculator/PayrollCalculator/src/main/java/org/example/Main.java
+++ workbook-3/PayrollCalculator/PayrollCalculator/src/main/java/org/example/Main.java
@@ -1,19 +1,64 @@
 package org.example;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
+public class Main
+{
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
     }
 
-    public static void employeeData(String[] args)
-    {
 
-        // get the specific variables
-        int employeeId = columns[0];
-        String name = columns[1];
-        double hoursWorked = Double.parseDouble(columns[2]);
-        double payRate = Double.parseDouble(columns[3]);
+    public static void calculateGrossPay()
+    {
+        // method that calculates and returns employee gross pay
+        int getGrossPay() = hoursWorked * payRate;
+        int displayGrossPay;
+    }
+
+    public static void loadEmployeeData()
+    {
+          // load a file reader object
+          // creates a path to the file
+          File file = new File("files/employeeData.csv");
+
+          // opens file using a Scanner
+          try(Scanner fileScanner = new Scanner(file))
+          {
+              // skip the first line - because it is a header line
+              fileScanner.nextLine();
+
+              // loops through each line in the file
+              while(fileScanner.hasNextLine())
+              {
+                  String line = fileScanner.nextLine();
+
+                  // split the line on a | (pipe)
+                  String[] columns = line.split("[|]]");
+
+                  // get the specific variables
+                  int employeeId = columns[0];
+                  String name = columns[1];
+                  double hoursWorked = Double.parseDouble(columns[2]);
+                  double payRate = Double.parseDouble(columns[3]);
+
+
+              }
+
+          }
+          catch (FileNotFoundException e)
+          {
+
+          }
+    }
+
+    public static void employee(int employeeId, String name, double hoursWorked, double payRate, double getGrossPay)
+    {
 
     }
 }

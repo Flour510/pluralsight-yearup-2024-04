10170e98d6cbb8b25137bc200da3a432e78ce9b6
flour <99916123+Flour510@users.noreply.github.com>
2024-06-11 07:17:29 -0700

    NorthwindTraders

--- workbook-8/Maven-Project/src/main/java/com/pluralsight/App.java
+++ workbook-8/Maven-Project/src/main/java/com/pluralsight/App.java
@@ -1,19 +1,19 @@
 package com.pluralsight;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 /**
  * Hello world!
- *
  */
+
 public class App 
 {
     final static Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
-
+        BasicConfigurator.configure();
         logMeLikeYouDo("☕");
-
     }
 
     private static void logMeLikeYouDo(String input){
@@ -29,7 +29,5 @@ public class App
         logger.warn("This is warn : " + input);
         logger.error("This is error : " + input);
         logger.fatal("This is fatal : " + input);
-
     }
-
 }

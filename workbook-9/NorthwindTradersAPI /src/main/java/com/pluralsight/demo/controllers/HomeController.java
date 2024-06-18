8df44012d5ad884cf19fab8b046ac08e4ce4eca2
flour <99916123+Flour510@users.noreply.github.com>
2024-06-18 16:26:29 -0700

    Pg.122 Fin

--- workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/HomeController.java
+++ workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/HomeController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
     // this method will respond to http://localhost:8080/
-    @RequestMapping(path="/", method= RequestMethod.GET) public String index(
-    @RequestParam(defaultValue="World") String name
-    ){
-        return "Hello " + name + "!";
+    @RequestMapping(path="/", method= RequestMethod.GET)
+    public String index(@RequestParam(name = "country", defaultValue = "World") String country) {
+        return "Hello " + country + "!";
     }
 }

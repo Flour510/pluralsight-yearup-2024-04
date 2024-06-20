c51d8a411637ed977b1cc0b0a101695900564ba4
flour <99916123+Flour510@users.noreply.github.com>
2024-06-20 15:09:08 -0700

    Pg. 138

--- workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/ProductsController.java
+++ workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/ProductsController.java
@@ -1,16 +1,26 @@
 package com.pluralsight.demo.controllers;
 
+import com.pluralsight.demo.models.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProductsController
 {
-     @GetMapping("/products")
-     public List<String> getAllProducts() {
-         return Arrays.asList("Chai", "Chang", "Aniseed Syrup", "Chef Anton's Cajun Seasoning");
-     }
+    // temporary list of categories
+    private List<Product> products = Arrays.asList(
+            new Product(1, "Chai", 1, 18.00),
+            new Product(2, "Chang", 1, 19.00),
+            new Product(3, "Aniseed Syrup", 2, 10.00),
+            new Product(4, "Chef Anton's Cajun Seasoning", 2, 22.00)
+    );
+
+    // return all categories
+    @GetMapping("/products")
+    public List<Product> getAllProducts()
+    {
+        return products;
+    }
 }

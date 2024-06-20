f448d1709f4b88b0fcff5c006692706fb6f1bf41
flour <99916123+Flour510@users.noreply.github.com>
2024-06-19 22:36:34 -0700

    Pg.138

--- workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/CategoriesController.java
+++ workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/CategoriesController.java
@@ -1,4 +1,16 @@
 package com.pluralsight.demo.controllers;
 
-public class CategoriesController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CategoriesController
+{
+    @GetMapping("/categories")
+    public List<String> getAllCategories() {
+        return Arrays.asList("Beverages", "Condiments", "Confections", "Dairy Products", "Grains/Cereals", "Meat/Poultry", "Produce", "Seafood");
+    }
 }
--- workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/ProductsController.java
+++ workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/controllers/ProductsController.java
@@ -1,6 +1,16 @@
 package com.pluralsight.demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class ProductsController
 {
-
+    @GetMapping("/products")
+    public List<String> getAllProducts() {
+        return Arrays.asList("Chai", "Chang", "Aniseed Syrup", "Chef Anton's Cajun Seasoning");
+    }
 }

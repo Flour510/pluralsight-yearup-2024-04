ab0dda0ee091b30ce992850a59e13d304f93aa74
flour <99916123+Flour510@users.noreply.github.com>
2024-06-20 22:38:43 -0700

    Pg.147 fin

--- workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/configurations/services/JdbcCategoryDao.java
+++ workbook-9/NorthwindTradersAPI /src/main/java/com/pluralsight/demo/configurations/services/JdbcCategoryDao.java
@@ -4,6 +4,11 @@ import com.pluralsight.demo.models.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -16,13 +21,52 @@ public class JdbcCategoryDao implements CategoryDao
 
     @Override
     public List<Category> getAll() {
-        // jdbc code to get all categories
-        return List.of();
+        List<Category> categories = new ArrayList<>();
+        String sql = "SELECT categoryId, categoryName FROM Categories";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery())
+        {
+            while (resultSet.next())
+            {
+                Category category = mapRowToProduct(resultSet);
+                categories.add(category);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categories;
     }
 
     @Override
     public Category getById(int id) {
-        // jdbc code to get a category by id
-        return null;
+        String sql = "SELECT categoryId, categoryName FROM Categories WHERE categoryId = ?";
+        Category category = null;
+
+        try (Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next())
+            {
+                category = mapRowToProduct(resultSet);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return category;
+    }
+
+    private Category mapRowToProduct(ResultSet resultSet) throws SQLException
+    {
+        int categoryId = resultSet.getInt("CategoryId");
+        String categoryName = resultSet.getString("CategoryName");
+
+        return new Category(categoryId, categoryName);
     }
 }
